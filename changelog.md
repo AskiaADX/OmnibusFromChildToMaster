b5d6b90ee2e09a74a592adb8a9a32ad21384aac6
Mamadou Sy <mamadou@askia.com>
2020-04-01 20:08:48 +0200

    Send the require information to complete the interview into the master survey

--- changelog.md
+++ changelog.md
@@ -1,3 +1,4 @@
 Version 1.0.0
 
 - Redirect to the master survey when the `Completed` question is not set
+- Send data to the master survey so it's possible to complete the current survey
--- resources/dynamic/script.js
+++ resources/dynamic/script.js
@@ -3,7 +3,14 @@
     setTimeout(function () {
         const shouldRedirect ={%= On(CurrentQuestion.Value HasNone {1}, "true", "false") %};
         if (shouldRedirect) {
-            window.location.href = "{%:= CurrentADC.Var("masterUrl") %}";
+            {% Dim suffix = CurrentADC.Var("surveySuffix") %}
+            const params = [
+                "Survey=" + encodeURIComponent(document.querySelector("input[name=Survey]").value),
+                "Intvw=" + encodeURIComponent(document.querySelector("input[name=Intvw]").value),
+                "Position=" + encodeURIComponent(document.querySelector("input[name=Position]").value),
+                encodeURIComponent("{%:= CurrentQuestion.InputName() %}") + "=" + encodeURIComponent("{%:= CurrentQuestion.AvailableResponses[1].InputValue() %}")
+           ];
+            window.location.href = "{%:= CurrentADC.Var("masterUrl") %}&state{%:= suffix %}=1&params{%:=suffix%}=" +encodeURIComponent(params.join("&"));
         } else {
             document.forms[0].submit();
         }
